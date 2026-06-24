f0d4f3a2ba380dbfae2e48cd63e72e5efef9bf44
PalashMendhe <palashmendhe777@gmail.com>
2026-06-25 02:29:42 +0530

    Created the world enviornment and the robots urdf.

---
+++ src/self_balancing_bot/launch/gazebo.launch.py
@@ -0,0 +1,68 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import Command
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    pkg_path = get_package_share_directory('self_balancing_bot')
+    urdf_file = os.path.join(pkg_path, 'urdf', 'robot.urdf.xacro')
+    robot_description = Command(['xacro', ' ', urdf_file])
+
+    return LaunchDescription([
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([
+                os.path.join(
+                    get_package_share_directory('ros_gz_sim'),
+                    'launch', 'gz_sim.launch.py'
+                )
+            ]),
+            launch_arguments={
+                'gz_args': '-r ' + os.path.join(
+                    get_package_share_directory('self_balancing_bot'),
+                    'worlds', 'basic_world.sdf'
+                )
+            }.items(),
+        ),
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            parameters=[{
+                'robot_description': robot_description,
+                'use_sim_time': True,
+            }],
+        ),
+        Node(
+            package='joint_state_publisher',
+            executable='joint_state_publisher',
+            name='joint_state_publisher',
+            parameters=[{'use_sim_time': True}],
+        ),
+        Node(
+            package='ros_gz_sim',
+            executable='create',
+            arguments=[
+                '-name', 'my_robot',
+                '-topic', '/robot_description',
+                '-x', '0.0',
+                '-y', '0.0',
+                '-z', '0.1',
+            ],
+            output='screen',
+        ),
+        Node(
+            package='ros_gz_bridge',
+            executable='parameter_bridge',
+            arguments=[
+                '/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock',
+                '/imu@sensor_msgs/msg/Imu[gz.msgs.IMU',
+                '/camera/image_raw@sensor_msgs/msg/Image[gz.msgs.Image',
+            ],
+            output='screen',
+        ),
+    ])
+
